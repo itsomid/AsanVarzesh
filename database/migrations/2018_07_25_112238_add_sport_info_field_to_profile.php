6bcc8c16320f911fd6a0dd21509bba51c7bd2bd0
Ali A. Jafari <aajafari87@gmail.com>
2018-07-25 18:25:31 +0430

    add Sport info fields to profile

--- database/migrations/2018_07_25_112238_add_sport_info_field_to_profile.php
+++ database/migrations/2018_07_25_112238_add_sport_info_field_to_profile.php
@@ -14,6 +14,22 @@ class AddSportInfoFieldToProfile extends Migration
     public function up()
     {
         //
+        Schema::table('profiles', function ($table) {
+
+            $table->enum('gender',['male','female'])->after('maim');
+            $table->string('birth-date')->nullable()->after('maim');
+            $table->integer('daily_activity')->nullable()->after('maim');
+            $table->json('selected_days_hours')->nullable()->after('maim');
+            $table->string('place_for_sport')->nullable()->after('maim');
+            $table->enum('level',['amateur','semi-professional','professional'])->after('maim');
+            $table->float('abdominal')->nullable()->after('maim');
+            $table->float('arm')->nullable()->after('maim');
+            $table->float('wrist')->nullable()->after('maim');
+            $table->float('hip')->nullable()->after('maim');
+            $table->float('waist')->nullable()->after('maim');
+            $table->text('target')->nullable()->after('maim');
+
+        });
     }
 
     /**
