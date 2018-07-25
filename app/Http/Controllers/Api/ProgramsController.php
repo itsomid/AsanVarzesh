3f8afefec7c45be84e03d3d35f7e36f780ad9842
Ali A. Jafari <aajafari87@gmail.com>
2018-07-25 20:52:10 +0430

    Add Program Model & Controller

---
+++ app/Http/Controllers/Api/ProgramsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProgramsController extends Controller
+{
+    //
+    public function store(Request $request) {
+
+        $data = $request->all();
+
+    }
+}
