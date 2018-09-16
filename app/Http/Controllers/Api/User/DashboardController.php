46d3fe0845de8a1dc5cf7be0c5d07e506657f216
Ali A. Jafari <aajafari87@gmail.com>
2018-09-16 14:04:35 +0430

    Change Dashboard user data by date

--- app/Http/Controllers/Api/User/DashboardController.php
+++ app/Http/Controllers/Api/User/DashboardController.php
@@ -14,58 +14,29 @@ class DashboardController extends Controller
         $user = auth('api')->user();
 
         if($date == null) {
-
-            $calendar_trainings = Calendar::where('user_id',$user->id)
-                ->with('training.accessories')
-                ->where('type','training')
-                ->orderby('id','DESC')
-                ->get()
-                ->groupBy('date')->toArray();
-            reset($calendar_trainings);
-            $key = key($calendar_trainings);
-
-            $trainings = $calendar_trainings[$key]; /* Final */
-
-            $calendar_nutrition = Calendar::where('user_id',$user->id)
-                ->with('package.foods')
-                ->where('type','package')
-                ->orderby('id','DESC')
-                ->get()
-                ->groupBy('date')->toArray();
-            reset($calendar_nutrition);
-            $key = key($calendar_nutrition);
-
-            $nutrition = $calendar_nutrition[$key]; /* Final */
-
-            return response()->json([
-                'trainings' => $trainings,
-                'nutrition' => $nutrition],200);
-
-
+            $date_carbon = Carbon::today();
         } else {
             $date_carbon = Carbon::parse($date);
-
-            $calendar_trainings = Calendar::where('user_id',$user->id)
-                ->with('training.accessories')
-                ->where('type','training')
-                ->where('date',$date_carbon)
-                ->orderby('id','DESC')
-                ->get()
-                ->toArray();
-
-            $calendar_nutrition = Calendar::where('user_id',$user->id)
-                ->with('training.accessories')
-                ->where('type','training')
-                ->where('date',$date_carbon)
-                ->orderby('id','DESC')
-                ->get()
-                ->toArray();
-
-            return response()->json([
-                'trainings' => $calendar_trainings,
-                'nutrition' => $calendar_nutrition],200);
-
         }
+        $calendar_trainings = Calendar::where('user_id',$user->id)
+            ->with('training.accessories')
+            ->where('type','training')
+            ->where('date',$date_carbon)
+            ->orderby('id','DESC')
+            ->get()
+            ->toArray();
+
+        $calendar_nutrition = Calendar::where('user_id',$user->id)
+            ->with('training.accessories')
+            ->where('type','training')
+            ->where('date',$date_carbon)
+            ->orderby('id','DESC')
+            ->get()
+            ->toArray();
+
+        return response()->json([
+            'trainings' => $calendar_trainings,
+            'nutrition' => $calendar_nutrition],200);
 
 
     }
