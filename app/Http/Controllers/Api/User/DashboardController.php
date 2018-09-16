e5b74de0b146e36bebfa4ce9d86eebe0a8d9cac0
Ali A. Jafari <aajafari87@gmail.com>
2018-09-16 14:33:34 +0430

    Fetch data in Dashboard by sport

--- app/Http/Controllers/Api/User/DashboardController.php
+++ app/Http/Controllers/Api/User/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\User;
 
 use App\Model\Calendar;
+use App\Model\Programs;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -18,14 +19,28 @@ class DashboardController extends Controller
         } else {
             $date_carbon = Carbon::parse($date);
         }
-        $calendar_trainings = Calendar::where('user_id',$user->id)
-            ->with(['training.accessories','training.sport'])
-            ->where('type','training')
-            ->where('date',$date_carbon)
-            ->orderby('id','DESC')
-            ->get()
-            ->toArray();
 
+
+        $programs = Programs::with('sport')->where('user_id',$user->id)->orderby('id','DESC')->get()->toArray();
+
+        $all_trainings_by_sport = [];
+        foreach ($programs as $program)
+        {
+//            $program['sport']['trainings'] = [];
+            $calendar_trainings = Calendar::where('user_id',$user->id)
+                ->with(['training.accessories','training.sport'])
+                ->where('type','training')
+                ->where('date',$date_carbon)
+                ->where('program_id',$program['id'])
+                ->orderby('id','DESC')
+                ->get()
+                ->toArray();
+            $program['sport']['trainings'] = $calendar_trainings;
+            array_push($all_trainings_by_sport,$program['sport']);
+
+
+        }
+        
         $calendar_nutrition = Calendar::where('user_id',$user->id)
             ->with('training.accessories')
             ->where('type','training')
@@ -35,7 +50,7 @@ class DashboardController extends Controller
             ->toArray();
 
         return response()->json([
-            'trainings' => $calendar_trainings,
+            'trainings' => $all_trainings_by_sport,
             'nutrition' => $calendar_nutrition],200);
 
 
