f39b10c374a7e5a567ee805d459af3df03982468
Ali A. Jafari <aajafari87@gmail.com>
2018-10-25 14:57:11 +0330

    change activity

--- app/Http/Controllers/Api/User/ActivityController.php
+++ app/Http/Controllers/Api/User/ActivityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\User;
 
 use App\Model\Activity;
+use App\Model\Calendar;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -24,13 +25,47 @@ class ActivityController extends Controller
 
         $data = $request->all();
 
-        $activity = new Activity();
-        $activity->user_id = $user->id;
-        $activity->distance = $data['distance'];
-        $activity->calorie = $data['calorie'];
-        $activity->time  = $data['time'];
-        $activity->calendar_id = $data['calendar_id'];
-        $activity->save();
+        if($data['calendar_id'] != null) {
+
+            $has_activity = Activity::where('calendar_id',$data['calendar_id'])->first();
+            if($has_activity != null OR $has_activity != '' OR !empty($has_activity) ) {
+                $activity = new Activity();
+                $activity->user_id = $user->id;
+                $activity->distance = $data['distance'];
+                $activity->energy = $data['energy'];
+                $activity->time  = $data['time'];
+                $activity->speed = $data['speed'];
+                $activity->calendar_id = $data['calendar_id'];
+                $activity->save();
+
+                if($data['calendar_id'] != null) {
+                    $calendar = Calendar::find($data['calendar_id']);
+                    $calendar->status = 'done';
+                    $calendar->save();
+                }
+
+                return response()->json(['message' => 'activity added'],200);
+            } else {
+                return response()->json(['message' => 'added before'],406);
+            }
+
+        } else {
+
+            $activity = new Activity();
+            $activity->user_id = $user->id;
+            $activity->distance = $data['distance'];
+            $activity->energy = $data['energy'];
+            $activity->time  = $data['time'];
+            $activity->speed = $data['speed'];
+            $activity->calendar_id = $data['calendar_id'];
+            $activity->save();
+
+            return response()->json(['message' => 'activity added'],200);
+
+
+        }
+
+
 
         return response()->json(['message' => 'activity added'],200);
 
