19ea1c3a8f5925b423ddc2da854078fb600f8436
Ali A. Jafari <aajafari87@gmail.com>
2019-01-07 13:16:55 +0330

    add pending count in program count

--- app/Http/Controllers/Api/Coach/SportTypeController.php
+++ app/Http/Controllers/Api/Coach/SportTypeController.php
@@ -18,7 +18,7 @@ class SportTypeController extends Controller
 
             $programs_count = Programs::where('sport_id',$sport->id)
                                         ->where('coach_id',1)
-                                        ->whereIn('status',['active','pending'])
+                                        ->whereIn('status',['active','pending','accept'])
                                         ->count();
             $sportArray = $sport->toArray();
             $sportArray['number_of_users'] = $programs_count;
