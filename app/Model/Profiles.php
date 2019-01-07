7658d6d99a21db84ed011d6c5d3c8557a17a7f70
Ali A. Jafari <aajafari87@gmail.com>
2019-01-07 09:13:25 +0330

    no message

--- app/Model/Profiles.php
+++ app/Model/Profiles.php
@@ -62,7 +62,7 @@ class Profiles extends Model
         $photos = [];
         if(isset($this->attributes['photos']) OR $this->attributes['photos'] != null) {
             foreach (\GuzzleHttp\json_decode($this->attributes['photos'],1) as $item) {
-                array_push($photos,Storage::url($item));
+                array_push($photos,url($item));
             }
             return $photos;
         }
