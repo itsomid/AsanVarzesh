3077043962cb4c70c393f2e2d3e22a40280312e0
Ali A. Jafari <aajafari87@gmail.com>
2019-05-02 16:32:46 +0430

    test gateway

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -21,4 +21,23 @@ class Helper
         return $string;
     }
 
+    public static function sendSMS($to,$msg)
+    {
+        $options = [
+            'verify' => false,
+            'timeout' => 0
+        ];
+        $url = "https://api.kavenegar.com/v1/6362557965644E54612B7951546679346734676B4B42576E58786969752B356A/sms/send.json";
+        $number = "1000596446";
+        $client = new \GuzzleHttp\Client($options);
+        $response = $client->request('POST', $url, [
+            'form_params' => [
+                'receptor' => $to,
+                'sender' => $number,
+                'message' => $msg
+            ]
+        ]);
+        return $response;
+    }
+
 }
