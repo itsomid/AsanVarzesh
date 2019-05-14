0bffd7eb884dc7e92a3ad9291570c2ee8f0d04c7
Ali A. Jafari <aajafari87@gmail.com>
2019-05-14 17:33:48 +0430

    fix requests

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -13,8 +13,8 @@ class Helper
 
     public function convert($input)
     {
-        $arabic = array('۰', '۱', '۲', '۳', '٤', '٥', '٦', '۷', '۸', '۹');
-        $persian = array('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹');
+        $persian = array('۰', '۱', '۲', '۳', '٤', '٥', '٦', '۷', '۸', '۹');
+        $arabic = array('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹');
         $english = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
 
         $string = str_replace($arabic, $english , $input);
