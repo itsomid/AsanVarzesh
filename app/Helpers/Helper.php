afbf82e2e699ff30a6c276839409fa60ce773373
Ali A. Jafari <aajafari87@gmail.com>
2019-05-14 17:28:45 +0430

    convert persian phone number to english number

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -13,10 +13,12 @@ class Helper
 
     public function convert($input)
     {
-        $unicode = array('۰', '۱', '۲', '۳', '٤', '٥', '٦', '۷', '۸', '۹');
+        $arabic = array('۰', '۱', '۲', '۳', '٤', '٥', '٦', '۷', '۸', '۹');
+        $persian = array('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹');
         $english = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
 
-        $string = str_replace($unicode, $english , $input);
+        $string = str_replace($arabic, $english , $input);
+        $string = str_replace($persian, $english , $string);
 
         return $string;
     }
