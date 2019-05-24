06417878e3ef5b1ac912323758ef0e82d1d5bc8e
Ali A. Jafari <aajafari87@gmail.com>
2019-05-24 16:43:13 +0430

    remove url func from nourlphotos attrib

--- app/Model/Profiles.php
+++ app/Model/Profiles.php
@@ -87,7 +87,7 @@ class Profiles extends Model
     public function getNourlphotosAttribute() {
         if(isset($this->attributes['photos']) OR $this->attributes['photos'] != null) {
             foreach (\GuzzleHttp\json_decode($this->attributes['photos'],1) as $item) {
-                array_push($photos,url($item));
+                array_push($photos,$item);
             }
             return $photos;
         }
