8489ae04f59cfe36ed1368a1b5de384f45c4fab2
Ali A. Jafari <aajafari87@gmail.com>
2019-07-11 01:19:37 +0430

    add has debit payment in callback

--- app/Http/Controllers/Api/User/PaymentController.php
+++ app/Http/Controllers/Api/User/PaymentController.php
@@ -53,98 +53,101 @@ class PaymentController extends Controller
         $referenceId = isset($data['referenceId']) ? $data['referenceId'] : 0;
         $gateway_status = 0;
 
-        if($data['resultCode'] == '100') {
-            // Verify
-            $result = $iranKish->verifyPayment($payment->token,$referenceId);
-            if(floatval($result) > 0 && floatval($result) == (floatval($payment->price))*10) {
+        $hasDebitPayment = Payment::where('program_id',$program->id)->where('type','debit')->first();
+        if(!$hasDebitPayment) {
+            if($data['resultCode'] == '100') {
+                // Verify
+                $result = $iranKish->verifyPayment($payment->token,$referenceId);
+                if(floatval($result) > 0 && floatval($result) == (floatval($payment->price))*10) {
 
-                // Successful Payment
-                $status = 'success';
-                $gateway_status = 100;
-                $message = 'پرداخت موفقیت آمیز';
+                    // Successful Payment
+                    $status = 'success';
+                    $gateway_status = 100;
+                    $message = 'پرداخت موفقیت آمیز';
 
-                $payment->status = $status;
-                $payment->reference_id = $referenceId;
-                $payment->gateway_status = $gateway_status;
-                $payment->gateway_message = $message;
-                $payment->save();
+                    $payment->status = $status;
+                    $payment->reference_id = $referenceId;
+                    $payment->gateway_status = $gateway_status;
+                    $payment->gateway_message = $message;
+                    $payment->save();
 
-                $debit_payment = new Payment();
-                $debit_payment->user_id = $program->user_id;
-                $debit_payment->coach_id = $program->coach_id;
-                $debit_payment->corrective_doctor_id = $coach->team['corrective_doctor'];
-                $debit_payment->nutrition_doctor_id = $coach->team['nutrition_doctor'];
-                $debit_payment->federation_id = $program->sport->federation->id;
-                $debit_payment->program_id = $program->id;
-                $debit_payment->price = $payment->price;
-                $debit_payment->type = 'debit';
-                $debit_payment->status = 'success';
-                $debit_payment->promotion_id = null;
-                $debit_payment->result_code = $data['resultCode'];
-                $debit_payment->save();
+                    $debit_payment = new Payment();
+                    $debit_payment->user_id = $program->user_id;
+                    $debit_payment->coach_id = $program->coach_id;
+                    $debit_payment->corrective_doctor_id = $coach->team['corrective_doctor'];
+                    $debit_payment->nutrition_doctor_id = $coach->team['nutrition_doctor'];
+                    $debit_payment->federation_id = $program->sport->federation->id;
+                    $debit_payment->program_id = $program->id;
+                    $debit_payment->price = $payment->price;
+                    $debit_payment->type = 'debit';
+                    $debit_payment->status = 'success';
+                    $debit_payment->promotion_id = null;
+                    $debit_payment->result_code = $data['resultCode'];
+                    $debit_payment->save();
 
-                // Subscription
-                $from = Carbon::today()->format('Y-m-d H:i:s');
-                $to = Carbon::today()->addDay(30);
-                $coach_sport = Coach_sport::where('sport_id',$program->sport_id)
-                    ->where('coach_id',$program->coach_id)
-                    ->first();
+                    // Subscription
+                    $from = Carbon::today()->format('Y-m-d H:i:s');
+                    $to = Carbon::today()->addDay(30);
+                    $coach_sport = Coach_sport::where('sport_id',$program->sport_id)
+                        ->where('coach_id',$program->coach_id)
+                        ->first();
 
-                $subscription = new Subscription();
-                $subscription->user_id = $program->user_id;
-                $subscription->from = $from;
-                $subscription->to = $to;
-                $subscription->program_id = $program->id;
-                $subscription->coach_sport_id = $coach_sport->id;
-                $subscription->save();
+                    $subscription = new Subscription();
+                    $subscription->user_id = $program->user_id;
+                    $subscription->from = $from;
+                    $subscription->to = $to;
+                    $subscription->program_id = $program->id;
+                    $subscription->coach_sport_id = $coach_sport->id;
+                    $subscription->save();
 
-                $program->status = 'pending';
-                $program->subscription_id = $subscription->id;
-                $program->save();
-
-
-
-                // Send Message To Coach
-                Helper::sendSMS($coach->mobile,'یک پرونده ورزشی جدید برای شما ارسال شد.');
+                    $program->status = 'pending';
+                    $program->subscription_id = $subscription->id;
+                    $program->save();
 
 
-            } else{
+
+                    // Send Message To Coach
+                    Helper::sendSMS($coach->mobile,'یک پرونده ورزشی جدید برای شما ارسال شد.');
+
+
+                } else {
+                    // Unsuccessfull Payment
+                    $status = 'failed';
+                    $gateway_status = $result;
+                    $message = $iranKish->lessThan100Message($result);
+
+                    $payment->gateway_status = $gateway_status;
+                    $payment->status = $status;
+                    $payment->gateway_message = $message;
+                    $payment->result_code = $data['resultCode'];
+                    $payment->reference_id = $referenceId;
+                    $payment->save();
+
+                    $program->status = 'cancel';
+                    $program->save();
+
+                }
+            } else {
                 // Unsuccessfull Payment
                 $status = 'failed';
-                $gateway_status = $result;
-                $message = $iranKish->lessThan100Message($result);
+                $gateway_status = $data['resultCode'];
+                $message = $iranKish->moreThan100messages($data['resultCode']);
 
                 $payment->gateway_status = $gateway_status;
                 $payment->status = $status;
                 $payment->gateway_message = $message;
                 $payment->result_code = $data['resultCode'];
-                $payment->reference_id = $referenceId;
                 $payment->save();
 
                 $program->status = 'cancel';
                 $program->save();
 
             }
-        } else {
-            // Unsuccessfull Payment
-            $status = 'failed';
-            $gateway_status = $data['resultCode'];
-            $message = $iranKish->moreThan100messages($data['resultCode']);
 
-            $payment->gateway_status = $gateway_status;
-            $payment->status = $status;
-            $payment->gateway_message = $message;
-            $payment->result_code = $data['resultCode'];
-            $payment->save();
 
-            $program->status = 'cancel';
-            $program->save();
 
+            return view('gateway_callback', ['payment' => $payment]);
         }
-
-
-
-        return view('gateway_callback', ['payment' => $payment]);
     }
 
     public function verifyPayment($reference_id,Payment $payment)
